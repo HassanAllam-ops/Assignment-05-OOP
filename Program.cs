ea19f1de799723d4f49a70798d189ec432af9191
Hassan Allam <hassan309pro@gmail.com>
2025-07-19 23:04:31 +0300

    Q1 : Classes And Interfaces

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
 ﻿namespace Assignment_05_OOP
 {
+    #region Q1
+    //public interface IShape
+    //{
+    //    double Area { get; }
+    //    void DisplayShapeInfo();
+    //}
+    //
+    //public interface ICircle : IShape
+    //{
+    //    double Redius { get; }
+    //}
+    //
+    //public interface IRectangle : IShape
+    //{
+    //    double Width { get; }
+    //    double Height { get; }
+    //}
+    //
+    //public class Circle : ICircle
+    //{
+    //    public double Redius { get; private set; }
+    //    public double Area => Math.PI * Redius * Redius;
+    //    public Circle (double redius)
+    //    {
+    //        Redius = redius;
+    //    }
+    //    public void DisplayShapeInfo()
+    //    {
+    //        Console.WriteLine($"Shape : Circle\n Redius : {Redius}\n Area : {Area}\n");
+    //    }
+    //}
+    //
+    //public class Rectangle : IRectangle
+    //{
+    //    public double Width { get; private set; }
+    //    public double Height { get; private set; }
+    //    public double Area => Width * Height;
+    //    public Rectangle (double width, double height)
+    //    {
+    //        Width = width;
+    //        Height = height;
+    //    }
+    //    public void DisplayShapeInfo()
+    //    {
+    //        Console.WriteLine($"Shape : Rectangle\n Width : {Width}\n Height : {Height}\n Area : {Area}");
+    //    }
+    //}
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implement Q1
+            //ICircle circle = new Circle(5.0);
+            //IRectangle rectangle = new Rectangle(5.0, 4.0);
+            //
+            //circle.DisplayShapeInfo();
+            //rectangle.DisplayShapeInfo();
+
+            #endregion
         }
     }
 }
